609255b0c34b2364eac4513ede69b05c3f5b6b93
Michael Johnston <mjohnst87@gmail.com>
2025-08-06 19:25:41 -0400

    status bar added

--- src/components/VibeWrappedScreen.js
+++ src/components/VibeWrappedScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import VibeScreen from './VibeScreen'; 
+import VibeScreen from './VibeScreen';
+import { StatusBar } from 'expo-status-bar';
+
 export default function VibeWrappedScreen(Component) {
   return (props) => (
     <VibeScreen>
+      <StatusBar style="light" backgroundColor="#000000" />
       <Component {...props} />
     </VibeScreen>
   );
